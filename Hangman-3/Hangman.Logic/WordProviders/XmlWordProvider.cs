7fb2b3d47ccbdce238d50d29e9a0b678f3e5ef61
GeriCookie <Gergana.Bailova@gmail.com>
2015-10-09 14:15:45 +0300

    Added tests for XmlWordProvider

--- Hangman-3/Hangman.Logic/WordProviders/XmlWordProvider.cs
+++ Hangman-3/Hangman.Logic/WordProviders/XmlWordProvider.cs
@@ -9,20 +9,35 @@ namespace Hangman.Logic.WordProviders
 {
     public class XmlWordProvider:BaseWordProvider
     {
+        private const string DefaultXmlWordsFilePath = @"..\..\..\Hangman.Logic\files\words.xml";
+
         private static volatile XmlWordProvider instance;
         private static object syncLock = new object();
      
-        private const string XmlWordsFilePath = @"..\..\..\Hangman.Logic\files\words.xml";
+        private string xmlWordsFilePath;
+
+        public string XmlWordsFilePath
+        {
+            private get
+            {
+                return this.xmlWordsFilePath;
+            }
+            set
+            {
+                this.xmlWordsFilePath = value;
+                this.AvailableWords = this.LoadWords();
+            }
+        }
            
         private XmlWordProvider()
         {
-            this.AvailableWords = this.LoadWords();
+            this.XmlWordsFilePath = DefaultXmlWordsFilePath;
             //this.AvailableWords = this.availableWords;
         }
 
         private IEnumerable<string> LoadWords()
         {
-            var doc = XDocument.Load(XmlWordsFilePath);
+            var doc = XDocument.Load(this.XmlWordsFilePath);
             return doc.Root.Elements().Select(element => element.Value.ToString());
         }
 
---
+++ Hangman-3/Hangman.Tests/WordProviders/XmlWordProviderTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Hangman.Tests.WordProviders
+{
+    //using Extensions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+    using System.Linq;
+    using System.Xml.Linq;
+
+    [TestClass]
+    public class XmlWordProviderTests
+    {
+        private string GetFilePathForWordsCount(int count)
+        {
+            var words = Enumerable.Range(0, count).Select(i => "Word" + i);
+            XDocument doc = new XDocument();
+
+            var root = new XElement("words");
+            doc.Add(root);
+
+            foreach (var word in words)
+            {
+                root.Add(new XElement("word", word));
+            }
+
+            var filePath = Path.GetTempFileName();
+            doc.Save(filePath);
+
+            return filePath;
+        }
+
+
+        [TestMethod]
+        public void XmlWordProviderGetWord_ManyWordsInFileAndCalledTwice_ShouldReturnDifferentWords()
+        {
+            var path = GetFilePathForWordsCount(15);
+
+            var b = 5;
+        }
+    }
+}
