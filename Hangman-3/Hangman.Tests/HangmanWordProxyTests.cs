7dd4cb024d48def63c381db3fdf81aae6e61a94f
Ivan Panchev <ivan.r.panchev@gmail.com>
2015-10-10 21:14:08 +0300

    Created a unit test class for the word proxy
    Also added the first two tests. We are invoking a private method in the
    proxy class with the Private Object.

---
+++ Hangman-3/Hangman.Tests/HangmanWordProxyTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Hangman.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class HangmanWordProxyTests
+    {
+        [TestMethod]
+        public void HangmanWordProxy_IsValidLetter_ShouldReturnFalseWhenInvalidLetterIsPassed()
+        {
+            var letter = "%";
+            PrivateObject hangmanWordProxy = new PrivateObject(typeof(Logic.HangmanWordProxy));
+
+            bool actual = Convert.ToBoolean(hangmanWordProxy.Invoke("IsValidLetter", letter));
+            bool expected = false;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HangmanWordProxy_IsValidLetter_ShouldReturnTrueWhenValidLetterIsPassed()
+        {
+            var letter = "a";
+            PrivateObject hangmanWordProxy = new PrivateObject(typeof(Logic.HangmanWordProxy));
+
+            bool actual = Convert.ToBoolean(hangmanWordProxy.Invoke("IsValidLetter", letter));
+            bool expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }   
+}
