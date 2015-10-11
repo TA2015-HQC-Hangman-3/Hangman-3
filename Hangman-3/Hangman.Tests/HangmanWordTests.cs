458e7642768194ebb2c533fcc21583250af19d28
U-MINKOV-PC\Jedi Master <gergana.bailova@gmail.com>
2015-10-11 03:17:39 +0300

    Added tests for HangmanWord

--- Hangman-3/Hangman.Tests/HangmanWordTests.cs
+++ Hangman-3/Hangman.Tests/HangmanWordTests.cs
@@ -21,20 +21,44 @@ namespace Hangman.Tests
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void HangmanWord_WhenNullWordIsProvidedShouldThrow()
+        public void HangmanWord_WhenNullWordIsProvided_ShouldThrow()
         {
             string word = null;
             var hangmanWord = new HangmanWord(word);
         }
 
         [TestMethod]
-        public void HangmanWord_ShouldGenerateHiddenWordReturnWordWithLenghtEqualToChosenWordWength()
+        public void HangmanWord_WhenWordIsValid_ShouldGenerateHiddenWordWithLenghtEqualToChosenWordLength()
         {
             string word = "cookie";
             var hangmanWord = new HangmanWord(word);
 
             Assert.AreEqual(hangmanWord.TheChosenWord.Length,hangmanWord.HiddenWord.Length);
-           
+        }
+
+        [TestMethod]
+        public void HangmanWordIsGuessedForTheFirstTime_WhenIsTheFirstTime_ShouldReturnTrue()
+        {
+            string word = "cookie";
+            var letter = "o";
+            var hangmanWord = new HangmanWord(word);
+
+            var actual = hangmanWord.IsLetterGuessedForFirstTime(letter);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void HangmanWordIsGuessedForTheFirstTime_WhenNotTheFirstTime_ShouldReturnFalse()
+        {
+            string word = "cookie";
+            var letter = "o";
+            var hangmanWord = new HangmanWord(word);
+            hangmanWord.IsLetterGuessedForFirstTime(letter);
+
+            var actual = hangmanWord.IsLetterGuessedForFirstTime(letter);
+
+            Assert.IsFalse(actual);
         }
     }
 }
