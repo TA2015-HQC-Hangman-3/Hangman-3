85b9cfb1434f90c62dff0356429f5b6beb0e6e5d
Dilyan Traykov <dtraykov94@gmail.com>
2015-10-12 11:31:03 +0300

    Added GameContext constructor test

---
+++ Hangman-3/Hangman.Tests/GameContextTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Hangman.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Hangman.Logic;
+    using Hangman.Logic.WordProviders;
+    using Hangman.Logic.Contracts;
+    using Moq;
+
+    [TestClass]
+    public class GameContextTests
+    {
+        [TestMethod]
+        public void GameContextConstructorShouldInitializeAllFieldsProperly()
+        {
+            var wordProvider = SimpleRandomWordProvider.Instance;
+            var scoreboard = new Mock<IScoreboard>();
+            var context = new GameContext(wordProvider, scoreboard.Object);
+            
+            Assert.IsNotNull(context.Word);
+            Assert.AreEqual(0, context.CurrentMistakes);
+            Assert.AreEqual(false, context.HasCheated);
+            Assert.AreEqual(true, context.IsGameRunning);
+        }
+    }
+}
